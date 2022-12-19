27073dc6a0f74825484d14b22a6d738f6dd002c3
Yallxe <82591945+yallxe@users.noreply.github.com>
2022-12-19 15:11:39 +0100

    cli ux fix

--- src/env.rs
+++ src/env.rs
@@ -6,16 +6,18 @@ pub fn get_hogg_dir() -> String {
     match std::env::var("HOGG_CONFIG_DIR") {
         Ok(path) => path,
         Err(_) => {
+            let default_path = ProjectDirs::from("", "", "Hogg").unwrap()
+                .config_dir().to_str().unwrap().to_string();
             unsafe {
                 if !WARN_PRINTED {
                     logs::info!(
-                        "HOGG_CONFIG_DIR environment variable is not set, using default path"
+                        "HOGG_CONFIG_DIR environment variable is not set, using {}",
+                        default_path
                     );
                     WARN_PRINTED = true;
                 }
             }
-            ProjectDirs::from("", "", "Hogg").unwrap()
-                .config_dir().to_str().unwrap().to_string()
+            default_path
         }
     }
 }
